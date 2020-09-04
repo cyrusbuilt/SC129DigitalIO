0da52312ca7c1bf9405930c9eb53c09a09936f58
cyrus <cyrusbuilt@gmail.com>
2020-09-04 09:00:49 -0400

    Increase test delay and add more verbosity

--- sc129tst.c
+++ sc129tst.c
@@ -5,7 +5,8 @@
 	output is tied to each input (I used jumpers). It then cycles through
 	all possible 8bit values and writes the value to the output port, then
 	reads the value back from the input port and compares to make sure the
-	written value matches the value read.
+	written value matches the value read. This effectively serves as unit
+	tests for the API as well as a diagnostic tool for the SC129.
 */
 
 #include "sc129.h"
@@ -13,17 +14,22 @@
 #include <stdlib.h>
 
 void main() {
-	printf("SC129 Test v1.0 by Cyrus Brunner\n\n");
+	printf("\n\nSC129 Test v1.0 by Cyrus Brunner\n\n");
+	printf("Make sure each output pin of the SC129 is connected to it's\n");
+	printf("corresponding input pin and then press any key to continue...\n");
+	fgetc_cons();
 	printf("Beginning SC129 test (values 0 - 255)...\n\n");
+	sleep(1);
 	SC129_setAddress(SC129_DEFAULT_ADDRESS);  // Change this to match the address the jumpers are set for.
 	
 	int val = 0;
 	for (int i = 0; i < 255; i++) {
 		SC129_write(i);
-		msleep(100);
+		msleep(500);
 		val = SC129_read();
+		printf("Output = %d, Input = %d - ", i, val);
 		if (val != i) {
-			printf("ERROR Value mismatch! O = %d, I = %d\n", i, val);
+			printf("ERROR Value mismatch!\n");
 		}
 		else {
 			printf("Value match!\n");
